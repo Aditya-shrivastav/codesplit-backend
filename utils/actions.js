5a0fa4421c5875c5da9df8114ff8ac4314665fc1
Aditya Shrivastav <aditya.shrivastav@shl.com>
2023-08-06 21:04:15 +0530

    initial commit

---
+++ utils/actions.js
@@ -0,0 +1,13 @@
+const Actions = {
+  JOIN: 'join',
+  JOINED: 'joined',
+  DISCONNECTED: 'disconnected',
+  CODE_CHANGE: 'code-change',
+  SYNC_CODE: 'sync-code',
+  SEND_MESSAGE: 'send-message',
+  RECEIVE_MESSAGE: 'receive-message',
+  SYNC_MESSAGE: 'sync-message',
+  LEAVE: 'leave',
+}
+
+module.exports = Actions
